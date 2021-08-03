d84cb7d19715785519917d765afdeb8d8e7bfae2
Tom Do <tom@sisuwellness.com>
2021-08-03 22:34:47 +1000

    master: init

---
+++ src/App.style.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const AppBox = styled.div`
+    text-align: center;
+`;
---
+++ src/comp/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeaderBox } from "./Header.style";
+
+export default function Header(){
+    return(
+        <HeaderBox>
+            <h1>Year Planner</h1>
+        </HeaderBox>
+    );
+}
