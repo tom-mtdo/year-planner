2b8435c5b72e261f4a37806bd8717ceeaf4dd97a
Tom Do <tom@sisuwellness.com>
2022-02-03 12:25:05 +1100

    hotfix: user note

---
+++ src/comp/3-footer/Footer.style.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledNote = styled.div`
+    position: absolute;
+    left: 0;
+    padding: 1em;
+    font-style: italic;
+`
--- src/comp/3-footer/Footer.tsx
+++ src/comp/3-footer/Footer.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Box from "../../lib/box/Box";
 import { StyledP } from '../comp.style';
+import { StyledNote } from "./Footer.style";
 
 export default function Footer(){
     return(
         <Box>
+            <StyledNote>* Double click on a day to add note</StyledNote>
             <StyledP>Copyright TOMTECH Pty Ltd</StyledP>
         </Box>
     );
